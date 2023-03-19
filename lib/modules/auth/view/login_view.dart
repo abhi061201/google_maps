84f9bdb48af1fc8f91f619c2b00efc39bd8771aa
abhi061201 <ag742757@gmail.com>
2023-03-19 13:18:23 +0530

    made loginUi

--- lib/modules/auth/view/login_view.dart
+++ lib/modules/auth/view/login_view.dart
@@ -37,75 +37,225 @@ class _login_viewState extends State<login_view> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login view'),
-        centerTitle: true,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Image(
+                    image: AssetImage(
+                      'assets/images/login_girl.png',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: Get.height * 0.05,
+                ),
+                Container(
+                  child: Column(children: [
+                    Text(
+                      'Welcome to Jhoice',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                    ),
+                    Text(
+                      'Create your account now',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                    ),
+                    SizedBox(
+                      height: Get.height * 0.035,
+                    ),
+                    Text(
+                      'Our registration process is quick and easy, taking',
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      'no more 10 minutes to complete',
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+                    ),
+                  ]),
+                ),
+                SizedBox(
+                  height: Get.height * 0.17,
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          debugPrint('Sign in is called');
+                          TruecallerSdk.initializeSDK(
+                              sdkOptions:
+                                  TruecallerSdkScope.SDK_OPTION_WITHOUT_OTP);
+                          // check krne k liye k truecaller install h bhi h ya nhi
+                          TruecallerSdk.isUsable.then((isUsable) {
+                            if (isUsable) {
+                              TruecallerSdk.getProfile;
+                            } else
+                              Get.snackbar(
+                                  "Error", "Please install truecaller");
+                          });
+                          // stream idhr ek truecaller vali stream ka obj h , isme saara data h
+                          stream = TruecallerSdk.streamCallbackData
+                              .listen((truecallerCallbackData) async {
+                            switch (truecallerCallbackData.result) {
+                              case TruecallerSdkCallbackResult.success:
+                                {
+                                  Map<String, dynamic> profileData = {};
+                                  profileData['first name'] =
+                                      truecallerCallbackData!
+                                          .profile!.firstName;
+                                  profileData['last name'] =
+                                      truecallerCallbackData!.profile!.lastName;
+                                  profileData['email'] =
+                                      truecallerCallbackData!.profile!.email;
+                                  profileData['avatar'] =
+                                      truecallerCallbackData.profile!.avatarUrl;
+                                  profileData['token'] =
+                                      truecallerCallbackData!.accessToken;
+                                  profileData['phoneNumber'] =
+                                      truecallerCallbackData!
+                                          .profile!.phoneNumber;
+
+                                  debugPrint('User data Success');
+                                  auth_controller.firebaseLogin(profileData);
+                                  break;
+                                }
+                              case TruecallerSdkCallbackResult.verification:
+                                {
+                                  Get.snackbar('Verification Required',
+                                      'Your Phone number needs to be verified.',
+                                      snackPosition: SnackPosition.BOTTOM);
+                                  Get.to(mobile_auth());
+                                  break;
+                                }
+                              default:
+                                {
+                                  Get.snackbar('Authentication failed',
+                                      "You have to login first",
+                                      snackPosition: SnackPosition.BOTTOM);
+                                  break;
+                                }
+                            }
+                          });
+                        },
+                        child: Container(
+                          width: 114,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(10),
+                                bottomLeft: Radius.circular(10)),
+                            color: Color(0xff28E9C8),
+                          ),
+                          child: Center(
+                              child: Text(
+                            'Sign in',
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white),
+                          )),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 1,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          debugPrint('Sign Up is called');
+                        },
+                        child: Container(
+                          width: 114,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(10),
+                                bottomRight: Radius.circular(10)),
+                            color: Color(0xff44F6D8),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                //   ElevatedButton(
+                //     onPressed: () async {
+                //       TruecallerSdk.initializeSDK(
+                //           sdkOptions: TruecallerSdkScope.SDK_OPTION_WITHOUT_OTP);
+                //       // check krne k liye k truecaller install h bhi h ya nhi
+                //       TruecallerSdk.isUsable.then((isUsable) {
+                //         if (isUsable) {
+                //           TruecallerSdk.getProfile;
+                //         } else
+                //           Get.snackbar("Error", "Please install truecaller");
+                //       });
+                //       // stream idhr ek truecaller vali stream ka obj h , isme saara data h
+                //       stream = TruecallerSdk.streamCallbackData
+                //           .listen((truecallerCallbackData) async {
+                //         switch (truecallerCallbackData.result) {
+                //           case TruecallerSdkCallbackResult.success:
+                //             {
+                //               Map<String, dynamic> profileData = {};
+                //               profileData['first name'] =
+                //                   truecallerCallbackData!.profile!.firstName;
+                //               profileData['last name'] =
+                //                   truecallerCallbackData!.profile!.lastName;
+                //               profileData['email'] =
+                //                   truecallerCallbackData!.profile!.email;
+                //               profileData['avatar'] =
+                //                   truecallerCallbackData.profile!.avatarUrl;
+                //               profileData['token'] =
+                //                   truecallerCallbackData!.accessToken;
+                //               profileData['phoneNumber'] =
+                //                   truecallerCallbackData!.profile!.phoneNumber;
+
+                //               debugPrint('User data Success');
+                //               auth_controller.firebaseLogin(profileData);
+                //               break;
+                //             }
+                //           case TruecallerSdkCallbackResult.verification:
+                //             {
+                //               Get.snackbar(
+                //                 'Verification Required',
+                //                 'Your Phone number needs to be verified.',
+                //               );
+                //               Get.to(mobile_auth());
+                //               break;
+                //             }
+                //           default:
+                //             {
+                //               Get.snackbar(
+                //                 'Authentication failed',
+                //                 "Error Code ${truecallerCallbackData.error!.code}",
+                //               );
+                //               break;
+                //             }
+                //         }
+                //       });
+                //     },
+                //     child: Text('Login Truecaller'),
+                //   )
+              ]),
+        ),
       ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                TruecallerSdk.initializeSDK(
-                    sdkOptions: TruecallerSdkScope.SDK_OPTION_WITHOUT_OTP);
-
-                // check krne k liye k truecaller install h bhi h ya nhi
-                TruecallerSdk.isUsable.then((isUsable) {
-                  if (isUsable) {
-                    TruecallerSdk.getProfile;
-                  } else
-                    Get.snackbar("Error", "Please install truecaller");
-                });
-
-                // stream idhr ek truecaller vali stream ka obj h , isme saara data h
-                stream = TruecallerSdk.streamCallbackData
-                    .listen((truecallerCallbackData) async {
-                  switch (truecallerCallbackData.result) {
-                    case TruecallerSdkCallbackResult.success:
-                      {
-                        Map<String, dynamic> profileData = {};
-                        profileData['first name'] =
-                            truecallerCallbackData!.profile!.firstName;
-                        profileData['last name'] =
-                            truecallerCallbackData!.profile!.lastName;
-                        profileData['email'] =
-                            truecallerCallbackData!.profile!.email;
-                        profileData['avatar'] =
-                            truecallerCallbackData.profile!.avatarUrl;
-                        profileData['token'] =
-                            truecallerCallbackData!.accessToken;
-                        profileData['phoneNumber'] =
-                            truecallerCallbackData!.profile!.phoneNumber;
-
-                        debugPrint('User data Success');
-                        auth_controller.firebaseLogin(profileData);
-                        break;
-                      }
-
-                    case TruecallerSdkCallbackResult.verification:
-                      {
-                        Get.snackbar(
-                          'Verification Required',
-                          'Your Phone number needs to be verified.',
-                        );
-                        Get.to(mobile_auth());
-                        break;
-                      }
-                    default:
-                      {
-                        Get.snackbar(
-                          'Authentication failed',
-                          "Error Code ${truecallerCallbackData.error!.code}",
-                        );
-                        break;
-                      }
-                  }
-                });
-              },
-              child: Text('Login Truecaller'),
-            )
-          ]),
     );
   }
 }
